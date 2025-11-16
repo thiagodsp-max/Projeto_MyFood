9e461e822ec3765d5348fbd28aa4207afbfa1f49
thiagodsp-max <tsp@ic.ufal.br>
2025-11-16 13:26:46 -0300

    Enhance Produto class with company ID
    Added company ID attribute and corresponding methods.

--- src/MyFood/Models/Produto.java
+++ src/MyFood/Models/Produto.java
@@ -3,8 +3,46 @@ package MyFood.Models;
 import MyFood.Exceptions.*;
 
 public class Produto {
-    int id;
+    int id; //id do produto
     String nome;
     float valor;
     String categoria;
+    int emp; //id da empresa
+
+    public Produto(int id,String name,float valor,String categ,int emp){
+        this.id=id;
+        this.nome=name;
+        this.valor=valor;
+        this.categoria=categ;
+        this.emp=emp;
+    }
+
+    //Métodos para chamar os Atributos do Produto
+    public String getNome(){
+        return this.nome;
+    }
+    public float getValor(){
+        return this.valor;
+    }
+    public String getCateg(){
+        return this.categoria;
+    }
+    //Métodos para a função de Editar Produto
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    public void setValor(float valor){
+        this.valor=valor;
+    }
+    public void setCateg(String categoria){
+        this.categoria=categoria;
+    }
+    //Id do Produto e Id da Empresa respectivamente
+    public int getId(){
+        return this.id;
+    }
+    public int getEmp(){
+        return this.emp;
+    }
+
 }
