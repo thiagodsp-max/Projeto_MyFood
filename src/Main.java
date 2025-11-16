adcfd293db776350f1a6f23e2648f32e816b88b4
thiagodsp-max <tsp@ic.ufal.br>
2025-11-16 13:12:38 -0300

    Refactor EasyAccept calls and update facade
    Updated the facade string and uncommented EasyAccept calls for multiple milestones.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 package src;
+import easyaccept.EasyAccept
 
 public class Main {
     public static void main(String[] args) {
-        //String facade = "br.ufal.ic.p2.wepayu.Facade";
+        String facade = "Facade";
         EasyAccept.main(new String[]{facade, "Milestone 01/us1_1.txt"});
-        //EasyAccept.main(new String[]{facade, "Milestone 01/us1_2.txt"});
-        //EasyAccept.main(new String[]{facade, "Milestone 01/us2_1.txt"});
-        //EasyAccept.main(new String[]{facade, "Milestone 01/us2_2.txt"});
-        //EasyAccept.main(new String[]{facade, "Milestone 01/us3_1.txt"});
-        //EasyAccept.main(new String[]{facade, "Milestone 01/us3_2.txt"});
-        //EasyAccept.main(new String[]{facade, "Milestone 01/us4_1.txt"});
-        //EasyAccept.main(new String[]{facade, "Milestone 01/us4_2.txt"});
+        EasyAccept.main(new String[]{facade, "Milestone 01/us1_2.txt"});
+        EasyAccept.main(new String[]{facade, "Milestone 01/us2_1.txt"});
+        EasyAccept.main(new String[]{facade, "Milestone 01/us2_2.txt"});
+        EasyAccept.main(new String[]{facade, "Milestone 01/us3_1.txt"});
+        EasyAccept.main(new String[]{facade, "Milestone 01/us3_2.txt"});
+        EasyAccept.main(new String[]{facade, "Milestone 01/us4_1.txt"});
+        EasyAccept.main(new String[]{facade, "Milestone 01/us4_2.txt"});
     }
 }
