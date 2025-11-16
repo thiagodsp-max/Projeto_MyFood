c39d50043155ddf05f7f27edf9262034ab6a8553
thiagodsp-max <tsp@ic.ufal.br>
2025-11-16 13:18:59 -0300

    Refactor Invalido exception for improved message
    Refactor Invalido exception class to simplify error message handling.

--- src/MyFood/Exceptions/Invalido.java
+++ src/MyFood/Exceptions/Invalido.java
@@ -1,27 +1,7 @@
-package MyFood.Exception;
+package MyFood.Exceptions;
 
-public class Invalido extends Exception{
-    public Invalido(String atr){
-        super(mensagem(atr));
-
-
-
-    public static String mensagem(String atr){
-        if(atr.equalsIgnoreCase("nome")){
-            return "Nome invalido";
-        }
-        else if(atr.equalsIgnoreCase("email")){
-            return "Email invalido";
-        }
-        else if(atr.equalsIgnoreCase("endereço")){
-            return "Endereco invalido";
-        }
-        else if(atr.equalsIgnoreCase("senha")){
-            return "Senha invalido";
-        }
-        else if(atr.equalsIgnoreCase("cpf")){
-            return "CPF invalido";
-        }
-        return null;
+public class Invalido extends Exception {
+    public Invalido(String atr) {
+        super(atr + " invalido");
     }
 }
