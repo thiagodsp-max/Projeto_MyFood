d77a7250f40e1dc78b59f50ac7bb3f9492a399a2
thiagodsp-max <tsp@ic.ufal.br>
2025-11-16 13:25:27 -0300

    Add owner attribute and constructor to Restaurante

--- src/MyFood/Models/Restaurante.java
+++ src/MyFood/Models/Restaurante.java
@@ -7,4 +7,30 @@ public class Restaurante {
     String nome;
     String endereco;
     String tipoCozinha;
+    Usuario dono;
+
+    public Restaurante(int ru, String name, String ender, String type, Usuario user){
+        this.id=ru;
+        this.endereco=ender;
+        this.nome=name;
+        this.tipoCozinha=type;
+        this.dono=user;
+    }
+
+    //Métodos para chamar os atributos da Empresa
+    public String getNome(){
+        return this.nome;
+    }
+    public String getEnder(){
+        return this.endereco;
+    }
+    public String getTipo(){
+        return this.tipoCozinha;
+    }
+    public int getId() {
+        return this.id;
+    }
+    public Usuario getDono(){
+        return this.dono;
+    }
 }
