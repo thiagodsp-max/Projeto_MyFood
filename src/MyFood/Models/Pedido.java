2449c2da4f13d399ef83c1397f747148c7c741b2
thiagodsp-max <tsp@ic.ufal.br>
2025-11-16 13:29:04 -0300

    Refactor Pedido class with new attributes and methods

--- src/MyFood/Models/Pedido.java
+++ src/MyFood/Models/Pedido.java
@@ -1,12 +1,60 @@
 package MyFood.Models;
 
 import MyFood.Exceptions.*;
+import java.util.*;
 
 public class Pedido {
     int numero;
+    int idcliente;
+    int idempresa;
     String cliente;
     String empresa;
-    String estado;
+    String status; //Aberto ou fechado
     //Lista de Produtos adicionados
-    float valor;
+    private List<Produto> buyed;
+
+    public Pedido(int num, Usuario cliente,Restaurante empresa){
+        this.numero=num;
+        this.idcliente=cliente.getId();
+        this.idempresa=empresa.getId();
+        this.cliente=cliente.getNome();
+        this.empresa=empresa.getNome();
+        this.status="aberto";
+        this.buyed=new ArrayList<>();
+    }
+
+    public int getNumero(){
+        return this.numero;
+    }
+    public int getIdcliente(){
+        return this.idcliente;
+    }
+    public int getIdempresa(){
+        return this.idempresa;
+    }
+    public String getCliente(){
+        return this.cliente;
+    }
+    public String getEmpresa(){
+        return this.empresa;
+    }
+    public String getEstado(){
+        return this.status;
+    }
+    //Retornar a Lista com os Nomes dos Produtos
+    public List<Produto> getProduto(){
+        return this.buyed;
+    }
+    //Fechar o Pedido
+    public void close(){
+        this.status="preparando";
+    }
+    //Pegar cada produto na lista, e somar o valor total a pagar
+    public float calcular(){
+        float total=0;
+        for(Produto z: buyed){
+            total+=z.getValor();
+        }
+        return total;
+    }
 }
